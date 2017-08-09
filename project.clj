0a109fd15bacaece4e342625f736c16921cdfca4
Matt Kocubinski <mkocubinski@gmail.com>
2017-08-09 18:23:20 -0400

    Version 0.1.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/sqlium "0.1.0"
+(defproject org.purefn/sqlium "0.1.1-SNAPSHOT"
   :description "A flexible, Datomic-inspired, config-driven Extraction system (big E in ETL)."
   :url "http://github.com/TheLadders/sqlium"
   :license {:name "Eclipse Public License"
